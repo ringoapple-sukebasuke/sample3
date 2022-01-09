e60ad6551f47339b715b57656efb7a7b3e498263
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-01-09 17:35:40 +0900

    投稿にユーザーを紐付ける

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_031503) do
+ActiveRecord::Schema.define(version: 2022_01_06_050210) do
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
