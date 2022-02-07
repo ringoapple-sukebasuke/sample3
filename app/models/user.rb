138aae5e22913264520b07c707df9501a15acf84
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-02-07 21:34:36 +0900

    投稿詳細ページが見辛かったため、色合い変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :companys, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def own?(object)
     id == object.user_id
