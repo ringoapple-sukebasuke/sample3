ddd8e2161d96ef8167a22c88e495baa7588cec5b
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-02-07 22:09:19 +0900

    companysが完成したため、posts不要に。posts関連ファイルを全てdestroy

--- app/models/post.rb
+++
@@ -1,9 +0,0 @@
-class Post < ApplicationRecord
-  validates :content, { presence: true, length: { maximum: 140 } }
-  validates :user_id, { presence: true}
-  belongs_to :user
-
-  def user
-    return User.find_by(id: self.user_id)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'index', to: 'home#index'
   get 'show', to: 'home#show'
-  resources :posts
   resources :companys do
     resources :comments, only: [:create]
   end
