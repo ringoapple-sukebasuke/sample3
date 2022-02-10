151a86d3a8fc3e89fa2d9dda26b08e2a2aa268d5
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-02-10 23:42:20 +0900

    User model テストコード完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, { presence: true, length: { maximum: 20 } }
-  validates :email, {presence: true, uniqueness: true}
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,11 +15,36 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:name]).to include(I18n.t('errors.messages.blank'))
   end
-  it "is invalid without a email"
-  
-  it "is invalid without a password"
 
-  it "is invalid with a duplicate email address"
+  it "is invalid without a email" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include(I18n.t('errors.messages.blank'))
+  end
 
-  it "return a user's name as a string"
+  it "is invalid without a password" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include(I18n.t('errors.messages.blank'))
+  end
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+      name: "ringo",
+      email: "ringo@gmail.com",
+      password: "123456",
+    )
+    user = User.new(
+      name: "ringo",
+      email: "ringo@gmail.com",
+      password: "123456",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("はすでに存在します")
+  end
+
+  it "return a user's name as a string" do
+    user = User.new(name: "ringo")
+    expect(user.name).to eq "ringo"
+  end
 end
