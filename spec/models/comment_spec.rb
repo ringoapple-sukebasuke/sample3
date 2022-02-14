7d018be92676ee143635c04461f50e07b68a3cdd
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-02-14 21:39:25 +0900

    comment model バリデーション、文字数テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.create(:comment)).to be_valid
+  end
+
+  it "is invalid without a body" do
+    comment = FactoryBot.build(:comment, body: nil)
+    comment.valid?
+    expect(comment.errors[:body]).to include(I18n.t('errors.messages.blank'))
+  end
+
+  it "bodyの文字数が1001文字以上の場合エラーメッセージを返す" do
+    comment = FactoryBot.create(:comment)
+    comment.body = Faker::Lorem.characters(number: 1001)
+    comment.valid?
+    expect(comment.errors[:body]).to include("は1000文字以内で入力してください")
+  end
 end
