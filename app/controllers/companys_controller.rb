c1442e83c4f5a9ff427feb32d347e1b60e413551
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-02-16 23:33:08 +0900

    requests/companys レスポンステスト（index）ログインしている場合といない場合

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -1,4 +1,5 @@
 class CompanysController < ApplicationController
+  before_action :authenticate_user!
   before_action :ensure_correct_user, { only: [:edit, :update, :destroy] }
 
   def index
--- spec/requests/companys_spec.rb
+++ spec/requests/companys_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Companys", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/companys/index"
-      expect(response).to have_http_status(:success)
+  describe "#index" do
+    context "as a authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      it "responds successfully returns a 200 response" do
+        sign_in @user
+        get companys_path
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+    end
+    context "as a guest" do
+      it "returns a 302 response" do
+        get companys_path
+        expect(response).to have_http_status "302"
+      end
+      it "redirects to the sign-in page" do
+        get companys_path
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 end
