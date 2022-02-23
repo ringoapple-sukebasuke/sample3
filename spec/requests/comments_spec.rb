ca69ae56676674a7b758991d92d7c661bd929479
ringoapple-sukebasuke <ringoapple.sukebasuke@gmail.com>
2022-02-23 18:45:21 +0900

    requests/comment(create)テスト作成

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) {FactoryBot.create(:user)}
+  let(:comment) {FactoryBot.create:comment, owner: (user)}
+  describe "#create" do
+    it "returns a 302 response" do
+      comment_params = FactoryBot.attributes_for(:comment)
+      post companys_path, params: { comment: comment_params }
+      expect(response).to have_http_status "302"
+    end
+    it " redirects to the sign-in page" do
+      comment_params = FactoryBot.attributes_for(:comment)
+      post companys_path, params: { comment: comment_params }
+      expect(response).to redirect_to "/users/sign_in"
+    end
   end
 end
